64f2f4c8957302688c89670b6a2f0ad5bd1ebc62
Andre Esteves <estevesandre1@hotmail.com>
2021-09-21 20:40:46 +0100

    Fix messages order

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
+const useStyles = makeStyles(() => ({
+  messagesBox: {
+    display: "flex",
+    flexDirection: "column-reverse",
+    height: 'calc(100% - 90px)',
+    paddingRight: '1rem',
+    overflowY: 'scroll',
+  }
+}));
+
 const Messages = (props) => {
+  const classes = useStyles();
   const { messages, otherUser, userId } = props;
 
   return (
-    <Box>
+    <Box className={classes.messagesBox}>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
