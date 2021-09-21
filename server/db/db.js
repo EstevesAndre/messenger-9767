82a7cb286a5abe407bb4831cf948f8ebefaf2ee2
Andre Esteves <estevesandre1@hotmail.com>
2021-09-21 12:06:54 +0100

    Update database connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,15 @@
+require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    port: 5432,
+    dialect: 'postgres',
+    logging: false
+  }
+);
 
 module.exports = db;
