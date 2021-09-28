dccfe05812439d4d7cb4d888eae76ecd48bc8d28
Andre Esteves <andreesteves111@gmail.com>
2021-09-28 13:35:38 +0100

    Prevent user converstations with the same name

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -3,6 +3,11 @@ const db = require("../db");
 const Message = require("./message");
 
 const Conversation = db.define("conversation", {
+  // user that created the conversation (to prevent conversation with the same name)
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
   // for conversations 1-1 the name can be username1_username2 or user1Id_user2Id
   // for group messages the name can be set
   name: {
@@ -12,13 +17,21 @@ const Conversation = db.define("conversation", {
 });
 
 // Through table
-const UserConversation = db.define("user_conversation", {});
+const UserConversation = db.define("user_conversation", {
+  lastMessageReadId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: null,
+  }
+});
 
-// find conversation given conversation name
-Conversation.findConversation = async function (name) {
+// find conversation given conversation userId and name
+Conversation.findConversation = async function (userId, name, creationDate) {
   const conversation = await Conversation.findOne({
     where: {
-      name: name
+      userId: userId,
+      name: name,
+      createdAt: creationDate,
     }
   });
 
